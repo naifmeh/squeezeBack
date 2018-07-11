3e2d27edd4067ff3c06dfa505255a1d094bdf713
tarzan212 <activation212@gmail.com>
2018-07-11 10:26:37 -0300

    Added variable path to scripts for better modularity

--- scripts/removePics.sh
+++ scripts/removePics.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PATH=/home/naif/Documents/squeezeCNN/training-images
+PATH=$SQUEEZEPATH/training-images
 
 for d in $PATH; do
 	if [ -d "$d" ]; then
--- scripts/trainNet.sh
+++ scripts/trainNet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #executing network
-cd /home/naif/Documents/squeezeCNN
+cd $SQUEEZEPATH
 rm aligned-images/cache.t7
 ./squeezecnn --train --align --align_folder_in=./training-images/ --align_folder_out=aligned-images
